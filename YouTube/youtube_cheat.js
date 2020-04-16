b615a9a6218980c935d480fa94fa047ea0eca61d
Karl <63649538+StasonJatham@users.noreply.github.com>
2020-04-17 01:20:39 +0200

    Create youtube_cheat.js

---
+++ YouTube/youtube_cheat.js
@@ -0,0 +1,447 @@
+var all_qa = {
+    "0": {
+        "question": "how would you update channel branding to support a tour announcement?",
+        "answer": ["upload a channel banner that includes the tour dates."]
+    },
+    "1": {
+        "question": "how do playlists affect watch time on a channel?",
+        "answer": ["playlists can increase watch time because viewers will often keep watching videos in a sequence."]
+    },
+    "2": {
+        "question": "what\u2019s something that video cards can promote?",
+        "answer": ["links to merch sites."]
+    },
+    "3": {
+        "question": "what does not describe a thumbnail that has been optimized for discovery?",
+        "answer": ["the thumbnail is&nbsp;low-resolution."]
+    },
+    "4": {
+        "question": "what does a channel\u2019s search data reveal?",
+        "answer": ["the keywords viewers are using to find the channel\u2019s videos."]
+    },
+    "5": {
+        "question": "how can subscribers boost engagement, and thus discovery, for a video?",
+        "answer": ["subscribers who have turned on notifications will generate the video\u2019s first wave of traffic."]
+    },
+    "6": {
+        "question": "what type of video is designed to get fans excited about a new album or tour?",
+        "answer": ["teasers or samplers."]
+    },
+    "7": {
+        "question": "why is behind-the-scenes footage from a tour valuable for a channel?",
+        "answer": ["all of the above."]
+    },
+    "8": {
+        "question": "what is the most effective way to promote a collaboration on youtube?",
+        "answer": ["produce a collaboration teaser and upload it to both collaborators\u2019 channels."]
+    },
+    "9": {
+        "question": "which of the following accurately describes art tracks?",
+        "answer": ["all of the above."]
+    },
+    "10": {
+        "question": "how does upgrading to an official artist channel affect an artist\u2019s subscribers?",
+        "answer": ["upgrading makes it easier for subscribers to find all of an artist\u2019s music in one place."]
+    },
+    "11": {
+        "question": "how does an artist benefit from upgrading to an official artist channel?",
+        "answer": ["the official artist channel appears at the top of search results for the artist."]
+    },
+    "12": {
+        "question": "what is the best way to leverage fan videos to grow a channel?",
+        "answer": ["drop fan videos into a playlist with a descriptive name."]
+    },
+    "13": {
+        "question": "what is the best way to engage fans before an album release on youtube?",
+        "answer": ["live stream a listening event."]
+    },
+    "14": {
+        "question": "why is it important to keep uploading videos after an album release?",
+        "answer": ["because it maintains or increases viewer engagement with the channel, which can increase album sales."]
+    },
+    "15": {
+        "question": "what is the most effective way to encourage subscribers to engage with a channel?",
+        "answer": ["upload videos on a regular schedule."]
+    },
+    "16": {
+        "question": "why might you want to pin a fan comment on a video?",
+        "answer": ["the comment says something positive about the video."]
+    },
+    "17": {
+        "question": "how can you use community to give fans an inside look at a tour?",
+        "answer": ["post a photo of the setlist."]
+    },
+    "18": {
+        "question": "with which youtube feature can you establish a watch page before a video is released, so that an artist and their fans can experience a new video together?",
+        "answer": ["premieres."]
+    },
+    "19": {
+        "question": "what is one effective way to use the community tab?",
+        "answer": ["post a mix of personal, promotional, and transactional posts to engage fans."]
+    },
+    "20": {
+        "question": "what can tell you about the global reach of a channel?",
+        "answer": ["geography data."]
+    },
+    "21": {
+        "question": "what can you do to prevent a sudden drop in a video\u2019s audience retention chart?",
+        "answer": ["ensure that the video\u2019s metadata accurately reflects the content."]
+    },
+    "22": {
+        "question": "what should you do when a channel\u2019s demographics data describes an audience that\u2019s much different from the targeted audience?",
+        "answer": ["revise the content strategy to focus on engaging the targeted audience."]
+    },
+    "23": {
+        "question": "youtube analytics for artists provides data about what type of content?",
+        "answer": ["an artist\u2019s songs across youtube."]
+    },
+    "24": {
+        "question": "what can you learn from the data in music charts & insights?",
+        "answer": ["the most popular music videos of the week in a given city or country."]
+    },
+    "25": {
+        "question": "what\u2019s one practical application for youtube analytics for artists?",
+        "answer": ["understand which of an artist\u2019s songs are fan favorites, and include these songs on a tour setlist."]
+    },
+    "26": {
+        "question": "if you wanted to learn in which major city a competing artist\u2019s video is getting the most views this week, what might help you?",
+        "answer": ["music charts and insights."]
+    },
+    "27": {
+        "question": "what requirement must a video meet before it\u2019s eligible to earn ad revenue?",
+        "answer": ["the content must be&nbsp;advertiser-friendly."]
+    },
+    "28": {
+        "question": "which two parties most commonly enter into an srav (sound recording audio visual) agreement with each other?",
+        "answer": ["youtube and record labels."]
+    },
+    "29": {
+        "question": "who can monetize videos on youtube?",
+        "answer": ["channels that have joined the youtube partner program, or channels that are linked to a label or distributor with an srav agreement."]
+    },
+    "30": {
+        "question": "what is the goal of the ad-serving model?",
+        "answer": ["to serve the&nbsp;highest-value&nbsp;ad to the right viewer at the right time."]
+    },
+    "31": {
+        "question": "what deters advertisers from showing ads on a video?",
+        "answer": ["explicit language."]
+    },
+    "32": {
+        "question": "why might you want to review the ratio of views to ads shown on a video in two time periods?",
+        "answer": ["to see how this ratio is trending over time."]
+    },
+    "33": {
+        "question": "how can you estimate which ad formats are earning the most revenue?",
+        "answer": ["use the ad rates report to compare the estimated revenue amounts generated from each ad type."]
+    },
+    "34": {
+        "question": "what low-investment strategy could a channel try to increase viewership (and ad revenue) in another country?",
+        "answer": ["leverage the audience to crowdsource captions and metadata translations."]
+    },
+    "35": {
+        "question": "why might two videos with similar monetizable view counts earn different revenue?",
+        "answer": ["different ad formats are enabled on the videos."]
+    },
+    "36": {
+        "question": "what can generate additional revenue for an artist on youtube?",
+        "answer": ["concert listings."]
+    },
+    "37": {
+        "question": "can an artist promote a paid product or service on youtube?",
+        "answer": ["yes, but the artist should be mindful of disclosure requirements and any&nbsp;third-party&nbsp;agreements."]
+    },
+    "38": {
+        "question": "what can you do to maximize an artist\u2019s revenue potential in the youtube music app?",
+        "answer": ["deliver the artist\u2019s entire catalog to youtube."]
+    },
+    "39": {
+        "question": "why should an artist seek legal advice before uploading a cover song?",
+        "answer": ["to determine whether the original song is protected by copyright."]
+    },
+    "40": {
+        "question": "which types of copyright apply most often to music on youtube?",
+        "answer": ["sound recording and musical composition."]
+    },
+    "41": {
+        "question": "what happens to a channel when it receives a copyright takedown notice?",
+        "answer": ["the offending video is removed and the channel owner receives a copyright strike."]
+    },
+    "42": {
+        "question": "what is subject to a \u201cmatch\u201d in content id?",
+        "answer": ["a video that contains content in the content id database."]
+    },
+    "43": {
+        "question": "what is the difference between copyright and content id?",
+        "answer": ["copyright is a set of laws, while content id is a tool that helps rights holders exercise their rights on youtube."]
+    },
+    "44": {
+        "question": "how does content id support the work of music rights holders?",
+        "answer": ["it enables them to monetize or otherwise protect their content on youtube."]
+    },
+    "45": {
+        "question": "why is it important to resolve asset ownership conflicts quickly?",
+        "answer": ["because conflicts can suspend any revenue generated from the videos in conflict."]
+    },
+    "46": {
+        "question": "which asset type is always linked to a sound recording asset on youtube?",
+        "answer": ["composition share."]
+    },
+    "47": {
+        "question": "what may indicate that a channel needs to clean up its reference files or revise its match policies?",
+        "answer": ["the channel sees an increase in claim disputes."]
+    },
+    "48": {
+        "question": "what predefined match policy allows viewers to watch a claimed video but does not allow ads to display on the video?",
+        "answer": ["track."]
+    },
+    "49": {
+        "question": "where can you see the relationship between references and assets?",
+        "answer": ["asset report."]
+    }
+}
+
+
+
+
+JaroWrinker  = function (s1, s2) {
+    var m = 0;
+
+    if ( s1.length === 0 || s2.length === 0 ) {
+        return 0;
+    };
+
+    if ( s1 === s2 ) {
+        return 1;
+    };
+
+    var range     = (Math.floor(Math.max(s1.length, s2.length) / 2)) - 1,
+        s1Matches = new Array(s1.length),
+        s2Matches = new Array(s2.length);
+
+    for ( i = 0; i < s1.length; i++ ) {
+        var low  = (i >= range) ? i - range : 0,
+            high = (i + range <= s2.length) ? (i + range) : (s2.length - 1);
+
+        for ( j = low; j <= high; j++ ) {
+        if ( s1Matches[i] !== true && s2Matches[j] !== true && s1[i] === s2[j] ) {
+            ++m;
+            s1Matches[i] = s2Matches[j] = true;
+            break;
+        };
+        };
+    };
+
+    if ( m === 0 ) {
+        return 0;
+    };
+
+    var k = n_trans = 0;
+
+    for ( i = 0; i < s1.length; i++ ) {
+        if ( s1Matches[i] === true ) {
+        for ( j = k; j < s2.length; j++ ) {
+            if ( s2Matches[j] === true ) {
+            k = j + 1;
+            break;
+            };
+        };
+
+        if ( s1[i] !== s2[j] ) {
+            ++n_trans;
+        };
+        };
+    };
+
+    var weight = (m / s1.length + m / s2.length + (m - (n_trans / 2)) / m) / 3,
+        l      = 0,
+        p      = 0.1;
+
+    if ( weight > 0.7 ) {
+        while ( s1[l] === s2[l] && l < 4 ) {
+        ++l;
+        };
+
+        weight = weight + l * p * (1 - weight);
+    };
+
+    return weight;
+};
+
+
+var answerFound = false;
+var questionTodo;
+var possibleAnswers = [];
+var answerCount = document.querySelectorAll("#assessment_response_form > div.question__answer > ul > li").length;
+
+if (document.querySelector('.checkbox__label')){
+
+    questionTodo    = document.querySelector("div.question__question > p").innerText.trim().toLowerCase();
+    possibleAnswers = document.querySelectorAll("#assessment_response_form > div.question__answer > ul > li");
+
+    var rightAnswers = matchQuestions(questionTodo,possibleAnswers);
+    document.querySelector('div.question__question > p').innerHTML = "<div style='border:1px solid black'><p>"+questionTodo+"</p><h3>I am "+(Math.floor(rightAnswers.score * 100))+"% sure.</h3><p>This is the question we matched:</p><p>"+rightAnswers.question+"</p><br><p>The originals question:<br>"+questionTodo+"</p><br><p>The following answers should be correct: <br><strong>"+rightAnswers.answer+"</strong></p></div>";
+
+
+    for (var x = 0; x < answerCount; x++){
+        var answerChoice = document.querySelector("#assessment_response_form > div.question__answer > ul > li:nth-child("+(x+1)+") > label > span.checkbox__label");
+        if(answerChoice){
+            for(var i = 0; i < rightAnswers.answer.length; i++){
+                if(rightAnswers.answer[i] === answerChoice.innerText.toLowerCase().trim()){
+                    answerFound = true;
+                    document.querySelector("#choice_id_"+x).click();
+                };
+            };
+        };
+    };
+
+    if (!answerFound){
+        var listFun = [];
+        var oldScore = 0;
+        var newScore = 0;
+        var oldAnswer;
+        var newAnswer;
+        var likelyAnswer = {
+            ourAnswer: "fuck you",
+            theirAnswer: questionTodo,
+            score: 0
+        };
+
+        for (var x = 0; x < answerCount; x++){
+            var answerChoice = document.querySelector("#assessment_response_form > div.question__answer > ul > li:nth-child("+(x+1)+") > label > span.checkbox__label");
+            if(answerChoice){
+                for(var y = 0; y < rightAnswers.answer.length; y++){
+                    var answ = answerChoice.innerText.toLowerCase();
+                    oldAnswer = rightAnswers.answer[y].toLowerCase();
+
+                    oldScore = JaroWrinker(rightAnswers.answer[y].toLowerCase(), answ);
+                    if(oldScore > newScore) {
+                        newScore = oldScore;
+                        newAnswer = oldAnswer;
+
+                        likelyAnswer = {
+                            ourAnswer: newAnswer.toLowerCase(),
+                            theirAnswer: answ,
+                            score: newScore
+                        };
+                        listFun.push(likelyAnswer);
+                    };
+                var sorted = listFun.sort(function(a,b,){
+                    return b.score - a.score;
+                });
+                };
+            };
+        };
+
+
+    var newSorted = sorted.slice(0, rightAnswers.answer.length);
+    var strOur = '<ul>';
+    newSorted.forEach(function(el) {
+        strOur += '<li>'+ el.ourAnswer + '</li>';
+    }); 
+    strOur += '</ul>';
+
+    var strTheir = '<ul>';
+    newSorted.forEach(function(el) {
+        strTheir += '<li>'+ el.theirAnswer + '</li>';
+    }); 
+    strTheir += '</ul>';
+
+    document.querySelector('div.question__question > p').innerHTML = "<div style='border:1px solid black'><p>"+questionTodo+"</p><h3>I am "+(Math.floor(likelyAnswer.score * 100))+"% sure.</h3><p>Their answer choice:</p><p>"+strTheir+"</p><p>Our answer: <strong>"+strOur+"</strong></p></div>";
+    
+    };
+
+} else {
+
+    questionTodo    = document.querySelector("div.question__question > p").innerText.trim().toLowerCase();
+    possibleAnswers = document.querySelectorAll("#assessment_response_form > div.question__answer > ul > li");
+
+    var rightAnswers = matchQuestions(questionTodo,possibleAnswers);
+
+    document.querySelector('div.question__question > p').innerHTML = "<div style='border:1px solid black'><p>"+questionTodo+"</p><h3>I am "+(Math.floor(rightAnswers.score * 100))+"% sure.</h3><p>This is the question we matched:</p><p>"+rightAnswers.question+"</p><br><p>The originals question:<br>"+questionTodo+"</p><br><p>The following answers should be correct: <br><strong>"+rightAnswers.answer+"</strong></p></div>";
+
+    for (var x = 0; x < answerCount; x++){
+        var answerChoice = document.querySelector("#assessment_response_form > div.question__answer > ul > li:nth-child("+(x+1)+") > label > span.radio__label");
+        if(answerChoice){
+            for(var i = 0; i < rightAnswers.answer.length; i++){
+                if(rightAnswers.answer[i].toLowerCase() === answerChoice.innerText.toLowerCase()){
+                    answerFound = true;
+                    document.querySelector("#choice_id_"+x).click();
+                };
+            };
+        };
+    };
+
+    if (!answerFound){
+        var oldScore = 0;
+        var newScore = 0;
+        var oldAnswer;
+        var newAnswer;
+        var likelyAnswer = {
+            ourAnswer: "fuck you",
+            theirAnswer: questionTodo,
+            score: 0
+        };
+
+        for (var x = 0; x < answerCount; x++){
+            var answerChoice = document.querySelector("#assessment_response_form > div.question__answer > ul > li:nth-child("+(x+1)+") > label > span.radio__label");
+            if(answerChoice){
+                for(var i = 0; i < rightAnswers.answer.length; i++){
+                    var answ = answerChoice.innerText.toLowerCase();
+                    oldAnswer = rightAnswers.answer[i].toLowerCase();
+
+                    oldScore = JaroWrinker(rightAnswers.answer[i].toLowerCase(), answ);
+
+                    if(oldScore > newScore) {
+                        newScore = oldScore;
+                        newAnswer = oldAnswer;
+
+                        likelyAnswer = {
+                            ourAnswer: newAnswer.toLowerCase(),
+                            theirAnswer: answ.toLowerCase(),
+                            score: newScore
+                        };
+                        document.querySelector("#choice_id_"+x).click();
+                    };
+                document.querySelector('div.question__question > p').innerHTML = "<div style='border:1px solid black'><p>"+questionTodo+"</p><h3>I am "+(Math.floor(likelyAnswer.score * 100))+"% sure.</h3><p>Their answer choice: <strong>"+likelyAnswer.theirAnswer+"</strong></p><p>Our answer: <strong>"+likelyAnswer.ourAnswer+"</strong></p>";
+                };
+            };
+        };
+    };
+
+}
+
+
+
+function matchQuestions(questionTodo, possibleAnswers){
+    var rightAnswers = {};
+    var questionInFile;
+    var answers;
+    var answerNum;
+    var mostLikely = {
+        score: 0.0,
+        question: "who the fuck?"
+    };
+    var oldScore = 0;
+    var newScore = 0;
+
+    for (var key in all_qa) {
+        if (all_qa.hasOwnProperty(key)) {
+    
+            questionInFile = all_qa[key].question;
+            answers  = all_qa[key].answer;
+            answerNum = answers.length;
+
+            oldScore = JaroWrinker(questionInFile.trim(), questionTodo.trim());
+            if(oldScore > newScore) {
+                newScore = oldScore;
+                var mostLikely = {
+                    score: newScore,
+                    question: questionInFile.toLowerCase(),
+                    answer: answers
+                };
+            };
+        };
+    };
+    return mostLikely;
+};
